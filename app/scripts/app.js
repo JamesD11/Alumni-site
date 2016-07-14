80838732ce5893028a8aad7244eaa09aa84696a5
James Davis <james.jpdavis@gmail.com>
2016-07-14 18:53:19 -0400

    removed require from app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-var api= require('db_routes.js');
+//var api= require('db_routes.js');
 angular
     .module("ngApp", ["ui.bootstrap", "ui.router"])
     .config(function($stateProvider, $urlRouterProvider) {
